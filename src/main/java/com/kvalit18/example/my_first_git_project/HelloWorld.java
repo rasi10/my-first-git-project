b8a3a6065bf6fdaaa666dc6338fb4ac3083595c1
Rafael Silva <rasilva.1986@gmail.com>
2018-12-12 11:55:41 +0100

    I changed the string that was printed from the program

--- src/main/java/com/kvalit18/example/my_first_git_project/HelloWorld.java
+++ src/main/java/com/kvalit18/example/my_first_git_project/HelloWorld.java
@@ -6,6 +6,6 @@ package com.kvalit18.example.my_first_git_project;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello, Kvalit18!");
 	}
 }
