8ec50e322d69c344d3dea2e6dc644e2455c04d32
Rafael Silva <rasilva.1986@gmail.com>
2018-12-12 11:52:02 +0100

    First commit

---
+++ src/main/java/com/kvalit18/example/my_first_git_project/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.kvalit18.example.my_first_git_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
